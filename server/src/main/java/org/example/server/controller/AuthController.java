bafd4f86d3f9622f3499b57b4af10f2708128a46
Asti Dhiya Anzaria <adanzaria1@sheffield.ac.uk>
2025-03-02 21:51:44 +0000

    feat: add oauth

--- server/src/main/java/org/example/server/controller/AuthController.java
+++ server/src/main/java/org/example/server/controller/AuthController.java
@@ -2,6 +2,11 @@ package org.example.server.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ **
+ * Google login path: http://localhost:8000/login/google
+ */
+
 @RestController
 public class AuthController {
 }
