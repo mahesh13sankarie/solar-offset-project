ae0c2b1452a5c6f6e0d103fc352fbd7c717c761e
Asti Dhiya Anzaria <adanzaria1@sheffield.ac.uk>
2025-03-02 21:15:45 +0000

    feat: add data structure

--- server/src/main/java/org/example/server/dto/UserDto.java
+++ server/src/main/java/org/example/server/dto/UserDto.java
@@ -1,4 +1,39 @@
 package org.example.server.dto;
 
+//TODO(): need to ensure data for standard account
 public class UserDto {
+    private String email, firstName, lastName, phoneNumber, password, address; //all fields mandatory
+
+    UserDto(String email, String firstName, String lastName, String phoneNumber, String password, String address) {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
 }
--- server/src/main/java/org/example/server/entity/User.java
+++ server/src/main/java/org/example/server/entity/User.java
@@ -1,4 +1,24 @@
 package org.example.server.entity;
 
+//TODO(): need to ensure data for standard account
 public class User {
+    String email, name, address;
+
+    User(String email, String name, String address) {
+        this.email = email;
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
