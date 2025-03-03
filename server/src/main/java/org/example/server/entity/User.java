2e309cbd86de6c3dd92ec1c1762a9a70df8a7f8a
Shreyas Desai <ssdesai1@sheffield.ac.uk>
2025-03-03 13:23:28 +0000

    MySQL Connection
    Code to connect to mysql and store user database, initialized table to store user information

--- server/src/main/java/org/example/server/entity/User.java
+++ server/src/main/java/org/example/server/entity/User.java
@@ -1,4 +1,94 @@
 package org.example.server.entity;
 
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private LocalDate loginDate;
+    private String email;
+    private String password;
+
+    public User(Long id, String firstName, String lastName, LocalDate loginDate, String email, String password) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.loginDate = loginDate;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User() {
+
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public LocalDate getLoginDate() {
+        return loginDate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setLoginDate(LocalDate loginDate) {
+        this.loginDate = loginDate;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", loginDate=" + loginDate +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
