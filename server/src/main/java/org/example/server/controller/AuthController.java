9a4a31f245e4a7eb37cd34f33ffd7764d1685964
Asti Dhiya Anzaria <adanzaria1@sheffield.ac.uk>
2025-03-03 15:23:13 +0000

    feat: user adjustment!

--- server/src/main/java/org/example/server/controller/AuthController.java
+++ server/src/main/java/org/example/server/controller/AuthController.java
@@ -1,12 +1,25 @@
 package org.example.server.controller;
 
+import org.example.server.dto.UserDto;
+import org.example.server.repository.UserRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  **
- * Google login path: http://localhost:8000/login/oauth2/code/google
+ * Google login path: <a href="http://localhost:8000/login/oauth2/code/google">...</a>
  */
 
 @RestController
 public class AuthController {
+    private UserRepository userRepository;
+
+    /*TODO: adjust mapping
+    @PostMapping("/signup")
+    ResponseEntity<?> signup(@RequestBody UserDto userDto){
+        return ResponseEntity.ok("200");
+    }
+    */
 }
--- server/src/main/java/org/example/server/service/auth/AuthService.java
+++ server/src/main/java/org/example/server/service/auth/AuthService.java
@@ -1,4 +1,7 @@
 package org.example.server.service.auth;
 
+import org.example.server.dto.UserDto;
+
 public interface AuthService {
+    UserDto saveUser(UserDto userDto);
 }
