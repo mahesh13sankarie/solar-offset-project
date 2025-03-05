db509582483d04121b10e68c1d860e1514fd61c3
Beomsu Koh <bkoh3@sheffield.ac.uk>
2025-03-05 21:46:55 +0000

    feat: add ElectricityMap WebClient and implementation

--- server/src/main/java/org/example/server/service/external/ElectricityMapWebClient.java
+++ server/src/main/java/org/example/server/service/external/ElectricityMapWebClient.java
@@ -1,4 +1,25 @@
 package org.example.server.service.external;
 
+import org.example.server.dto.CarbonIntensityResponseDTO;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
 public class ElectricityMapWebClient {
+
+    private final WebClient webClient;
+
+    public ElectricityMapWebClient(@Qualifier("electricityMapApiWebClient") WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    public CarbonIntensityResponseDTO fetchCarbonIntensity(String token) {
+        return webClient.get()
+                .uri("/v3/carbon-intensity/latest")
+                .header("auth-token", token)
+                .retrieve()
+                .bodyToMono(CarbonIntensityResponseDTO.class)
+                .block();
+    }
 }
