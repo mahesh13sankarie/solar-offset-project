3b8dfae08d3c634cc048962ec67feaabd4b3f150
Beomsu Koh <bkoh3@sheffield.ac.uk>
2025-03-05 21:45:06 +0000

    feat: add WebClient configuration for electricity map api

--- server/src/main/java/org/example/server/config/WebClientConfig.java
+++ server/src/main/java/org/example/server/config/WebClientConfig.java
@@ -1,4 +1,21 @@
 package org.example.server.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
 public class WebClientConfig {
+
+    // Latest Carbon Intensity
+    // https://portal.electricitymaps.com/docs/api#latest-carbon-intensity
+    @Bean(name ="electricityMapApiWebClient")
+    public WebClient webClient() {
+        return WebClient.builder()
+                .baseUrl("https://api.electricitymap.org")
+                .defaultHeader(HttpHeaders.AUTHORIZATION, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+    }
 }
