f53f6c3223c15eec9eae860c6ecc908df0f44345
Shreyas Desai <ssdesai1@sheffield.ac.uk>
2025-03-05 23:37:23 +0000

    Save login page data to database

---
+++ server/src/main/java/org/example/server/config/CorsConfig.java
@@ -0,0 +1,17 @@
+package org.example.server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+            }
+        };
+    }
+}
