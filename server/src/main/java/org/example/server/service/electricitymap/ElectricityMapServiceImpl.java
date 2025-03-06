9535b7536f3203b0a37deccde8978459bcd47d5e
Beomsu Koh <bkoh3@sheffield.ac.uk>
2025-03-06 21:52:45 +0000

    fix: update electricity map api request and response handling with integration test

--- server/src/main/java/org/example/server/service/electricitymap/ElectricityMapServiceImpl.java
+++ server/src/main/java/org/example/server/service/electricitymap/ElectricityMapServiceImpl.java
@@ -2,42 +2,46 @@ package org.example.server.service.electricitymap;
 
 import lombok.RequiredArgsConstructor;
 import org.example.server.dto.CarbonIntensityResponseDTO;
+import org.example.server.dto.ElectricityMapCredentialDTO;
+import org.example.server.dto.ElectricityMapPropertiesDTO;
 import org.example.server.entity.CarbonIntensity;
 import org.example.server.repository.ElectricityMapRepository;
 import org.example.server.service.external.ElectricityMapWebClient;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
+@EnableConfigurationProperties(ElectricityMapPropertiesDTO.class)
 public class ElectricityMapServiceImpl implements ElectricityMapService {
-
 
     private final ElectricityMapWebClient electricityMapWebClient;
     private final ElectricityMapRepository electricityMapRepository;
 
     // Tokens loaded from application.yml for API calls
-    @Value("${electricityMap.tokens}")
-    private String[] tokens;
+    private final ElectricityMapPropertiesDTO properties;
 
     // Scheduler to execute every hour (fixedRate: 3600000 milliseconds)
     // Loop sends API requests 5 times if tokens array contains 5 tokens
     @Scheduled(fixedRate = 3600000)
     public void fetchAndSaveElectricityData() {
-        for (String token : tokens) {
-            fetchAndSaveElectricityDataAsync(token);
+        List<ElectricityMapCredentialDTO> credentials = properties.getCredentials();
+        for (ElectricityMapCredentialDTO credential : credentials) {
+            fetchAndSaveElectricityDataAsync(credential);
         }
     }
 
     // Asynchronous method to process each API call concurrently
     @Async
-    public void fetchAndSaveElectricityDataAsync(String token) {
-        CarbonIntensityResponseDTO response = electricityMapWebClient.fetchCarbonIntensity(token);
+    public void fetchAndSaveElectricityDataAsync(ElectricityMapCredentialDTO credential) {
+        CarbonIntensityResponseDTO response = electricityMapWebClient.fetchCarbonIntensity(credential);
         CarbonIntensity entity = CarbonIntensity.builder()
                 .carbonIntensity(response.carbonIntensity())
-                .countryCode(response.countryCode())
+                .countryCode(response.zone())
                 .datetime(response.datetime())
                 .updatedAt(response.updatedAt())
                 .build();
