94c1308c94faf60ee05dfe80a4b3e7f14f3f0109
Beomsu Koh <bkoh3@sheffield.ac.uk>
2025-03-06 20:46:37 +0000

    feat: hourly carbon data update using async fetch

--- server/src/main/java/org/example/server/service/electricitymap/ElectricityMapServiceImpl.java
+++ server/src/main/java/org/example/server/service/electricitymap/ElectricityMapServiceImpl.java
@@ -6,6 +6,7 @@ import org.example.server.entity.CarbonIntensity;
 import org.example.server.repository.ElectricityMapRepository;
 import org.example.server.service.external.ElectricityMapWebClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -21,18 +22,25 @@ public class ElectricityMapServiceImpl implements ElectricityMapService {
     @Value("${electricityMap.tokens}")
     private String[] tokens;
 
+    // Scheduler to execute every hour (fixedRate: 3600000 milliseconds)
+    // Loop sends API requests 5 times if tokens array contains 5 tokens
     @Scheduled(fixedRate = 3600000)
     public void fetchAndSaveElectricityData() {
-        String token = ""; // Todo: add secret api key
+        for (String token : tokens) {
+            fetchAndSaveElectricityDataAsync(token);
+        }
+    }
+
+    // Asynchronous method to process each API call concurrently
+    @Async
+    public void fetchAndSaveElectricityDataAsync(String token) {
         CarbonIntensityResponseDTO response = electricityMapWebClient.fetchCarbonIntensity(token);
         CarbonIntensity entity = CarbonIntensity.builder()
                 .carbonIntensity(response.carbonIntensity())
                 .countryCode(response.countryCode())
                 .datetime(response.datetime())
                 .updatedAt(response.updatedAt())
-            .build();
-
+                .build();
         electricityMapRepository.save(entity);
     }
-
 }
