d272fa00533fea2ffd1595adcf579c9159661de9
Shreyas Desai <ssdesai1@sheffield.ac.uk>
2025-03-11 11:34:15 +0000

    Revert "Merge branch 'revert-cecc2f3e' into 'shreyas/v2/frontend'"
    This reverts merge request !11

---
+++ server/src/main/java/org/example/server/controller/UserController.java
@@ -0,0 +1,26 @@
+package org.example.server.controller;
+import org.example.server.entity.User;
+import org.example.server.repository.UserRepository;
+import org.example.server.service.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+@RestController
+@RequestMapping("/api/register")
+//@CrossOrigin(origins = "http://127.0.0.1:5500") // Allow frontend to connect
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public String registerUser(@RequestBody User user) {
+
+        try {
+            userService.registerUser(user);
+            return "success user registered";
+        }
+        catch (Exception e) {
+            return "Error: " +  e.getMessage();
+        }
+    }
+}
---
+++ server/src/main/java/org/example/server/security/SecurityConfig.java
@@ -0,0 +1,4 @@
+package org.example.server.security;
+
+public class SecurityConfig {
+}
