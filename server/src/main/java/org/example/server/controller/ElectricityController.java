aa0c9da1e5bf377e9681ac6e7d11d3eaf7cf3dc2
Beomsu Koh <bkoh3@sheffield.ac.uk>
2025-03-17 12:33:52 +0000

    fix : missing api version

--- server/src/main/java/org/example/server/controller/ElectricityController.java
+++ server/src/main/java/org/example/server/controller/ElectricityController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/electricity")
+@RequestMapping("/api/v1/electricity")
 public class ElectricityController {
 
     private final ElectricityMetricsService electricityMetricsService;
