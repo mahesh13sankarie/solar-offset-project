8cdc0d2119024da518af8c19351b5d4261030022
Beomsu Koh <bkoh3@sheffield.ac.uk>
2025-03-30 23:04:34 +0100

    refactor : add Stripe Exception into service

--- server/src/main/java/org/example/server/service/Payment/PaymentService.java
+++ server/src/main/java/org/example/server/service/Payment/PaymentService.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.example.server.dto.PaymentRequestDTO;
 import org.example.server.dto.PaymentResponseDTO;
 
+import com.stripe.exception.StripeException;
+
 public interface PaymentService {
 
-	PaymentResponseDTO processPayment(PaymentRequestDTO request);
+	PaymentResponseDTO processPayment(PaymentRequestDTO request) throws StripeException;
 
-	List<PaymentResponseDTO> getUserPayments(Long userId);
+	List<PaymentResponseDTO> getUserPayments(Long userId) throws StripeException;
 }
