bf05b27bc1ef5bffd4ea001fcef3807fd5cb76ae
Asti Dhiya Anzaria <adanzaria1@sheffield.ac.uk>
2025-04-01 17:11:58 +0100

    feat: add new endpoint for admin

--- server/src/main/java/org/example/server/controller/AdminController.java
+++ server/src/main/java/org/example/server/controller/AdminController.java
@@ -4,10 +4,7 @@ import org.example.server.mapper.AuthResponseMapper;
 import org.example.server.service.auth.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author: astidhiyaa
@@ -27,5 +24,15 @@ public class AdminController {
     @GetMapping("/users")
     public ResponseEntity<?> fetchUser() {
         return ResponseEntity.ok().body(responseMapper.buildCustomResponse(authService.getUsers()));
+    }
+
+    @PutMapping("/update-role")
+    public ResponseEntity<?> updateRole() {
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/delete-user")
+    public ResponseEntity<?> deleteUser() {
+        return ResponseEntity.ok().build();
     }
 }
