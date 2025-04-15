483e07148e81ded806803af7af3b1d878510cc42
Shreyas Desai <ssdesai1@sheffield.ac.uk>
2025-04-15 12:24:37 +0100

    Fix: Incorrect component render error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,7 @@ import SolarComparison from "./components/LandingPage/SolarComparison.jsx";
 import Services from "./components/LandingPage/Services.jsx";
 import Navbar from "./components/LandingPage/Navbar.jsx";
 import InstallationCost from "./components/LandingPage/InstallationCost.jsx"
-
+import ReportGenerate from "./components/Report/ReportGenerate.jsx";
 
 import AdminLogin from './components/Login/AdminLogin.jsx';
 import SideNav from "./components/Dashboard/Sidebar.jsx";
@@ -20,13 +20,11 @@ import Dashboard from "./components/Dashboard/Dashboard.jsx";
 import Userpage from "./components/Dashboard/UserSection.jsx";
 import ReportDashboard from "./components/Report/ReportDashboard.jsx";
 import Invoice from "./components/Report/Invoice/Invoice.jsx";
-import ReportGenerate from "./components/Dashboard/ReportGenerate.jsx";
-import Payment from "./components/LandingPage/Payment.jsx";
 
 const router = createBrowserRouter([
-  {path:"/",element:<Home />},
-  {path:"/login",element:<AuthForm />},
-  {path:"/admin",element:<AdminLogin />},
+    {path:"/",element:<Home />},
+    {path:"/login",element:<AuthForm />},
+    {path:"/admin",element:<AdminLogin />},
     {
         path:"/dashboard",
         element:<Dashboard />,
@@ -35,11 +33,17 @@ const router = createBrowserRouter([
             {path:"/dashboard/reports",element:<ReportGenerate />}
         ]
     },
-  {path:"/login",element:<AuthForm />},
+    {path:"/login",element:<AuthForm />},
     {path:"/SolarComparison", element:<SolarComparison/>},
     {path:"/navbar",element:<Navbar />},
     {path:"/donate",element:<Services />},
-    {path:"/InstallationCost/:countryCode", element:<InstallationCost/>}
+    {path:"/InstallationCost/:countryCode", element:<InstallationCost/>},
+    {
+        path:"/staff/dashboard",element:<ReportDashboard />
+
+    },
+    {path:"/pdf", element:<ReportGenerate />},
+
 ])
 
 
