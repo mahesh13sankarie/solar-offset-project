8b34c9083f86934090966e84fbfab6fc5a473ebc
Your Name <mperiyasamynallasamy1@sheffield.ac.uk>
2025-04-16 01:33:05 +0100

    Resolved issue in paymentpage and changepassword

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,8 @@ import SolarComparison from "./components/LandingPage/SolarComparison.jsx";
 import Services from "./components/LandingPage/Services.jsx";
 import Navbar from "./components/LandingPage/Navbar.jsx";
 import InstallationCost from "./components/LandingPage/InstallationCost.jsx"
+import ChangePassword from "./components/Login/ChangePassword.jsx";
+
 
 
 import AdminLogin from './components/Login/AdminLogin.jsx';
@@ -20,27 +22,31 @@ import Dashboard from "./components/Dashboard/Dashboard.jsx";
 import Userpage from "./components/Dashboard/UserSection.jsx";
 import ReportDashboard from "./components/Report/ReportDashboard.jsx";
 import Invoice from "./components/Report/Invoice/Invoice.jsx";
-import ReportGenerate from "./components/Dashboard/ReportGenerate.jsx";
+import ReportGenerate from "./components/Report/ReportGenerate.jsx";
 import Payment from "./components/LandingPage/Payment.jsx";
 
 const router = createBrowserRouter([
-  {path:"/",element:<Home />},
-  {path:"/login",element:<AuthForm />},
-  {path:"/admin",element:<AdminLogin />},
-    {
-        path:"/dashboard",
-        element:<Dashboard />,
-        children:[
-            {path:"/dashboard/users",element:<Userpage />},
-            {path:"/dashboard/reports",element:<ReportGenerate />}
-        ]
-    },
-  {path:"/login",element:<AuthForm />},
-    {path:"/SolarComparison", element:<SolarComparison/>},
-    {path:"/navbar",element:<Navbar />},
-    {path:"/donate",element:<Services />},
-    {path:"/InstallationCost/:countryCode", element:<InstallationCost/>}
-])
+  { path: "/", element: <Home /> },
+  { path: "/login", element: <AuthForm /> },
+  { path: "/admin", element: <AdminLogin /> },
+  { path: "/SolarComparison", element: <SolarComparison /> },
+  { path: "/donate", element: <Services /> },
+  { path: "/InstallationCost/:countryCode", element: <InstallationCost /> },
+  { path: "/staff/dashboard", element: <ReportDashboard /> },
+  { path: "/pdf", element: <ReportGenerate /> },
+  { path: "/change-password", element: <ChangePassword /> },
+  { path: "/Payment/:countryCode", element: <Payment /> },
+
+
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+    children: [
+      { path: "/dashboard/users", element: <Userpage /> },
+      { path: "/dashboard/reports", element: <ReportGenerate /> },
+    ],
+  },
+]);
 
 
 const App = () => {
