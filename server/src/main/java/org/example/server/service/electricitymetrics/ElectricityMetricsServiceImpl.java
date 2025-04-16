0462ab06e1ec82e5e0f5d825aef3410231eedc01
Beomsu Koh <bkoh3@sheffield.ac.uk>
2025-04-16 18:46:22 +0100

    fix : unmatched panel logic

--- server/src/main/java/org/example/server/service/electricitymetrics/ElectricityMetricsServiceImpl.java
+++ server/src/main/java/org/example/server/service/electricitymetrics/ElectricityMetricsServiceImpl.java
@@ -1,14 +1,13 @@
 package org.example.server.service.electricitymetrics;
 
 import org.example.server.dto.CountryDetailDTO;
+import org.example.server.dto.CountryPanelDTO;
 import org.example.server.dto.SolarPanelDTO;
-import org.example.server.entity.CarbonIntensity;
-import org.example.server.entity.Country;
-import org.example.server.entity.ElectricityBreakdown;
-import org.example.server.entity.Panel;
+import org.example.server.entity.*;
 import org.example.server.exception.DataNotFoundException;
 import org.example.server.mapper.ElectricityMetricsMapper;
 import org.example.server.repository.CarbonIntensityRepository;
+import org.example.server.repository.CountryPanelRepository;
 import org.example.server.repository.CountryRepository;
 import org.example.server.repository.ElectricityBreakdownRepository;
 import org.example.server.utils.CalculationUtils;
@@ -26,15 +25,17 @@ public class ElectricityMetricsServiceImpl implements ElectricityMetricsService 
     private final ElectricityBreakdownRepository electricityBreakdownRepository;
     private final ElectricityMetricsMapper electricityMetricsMapper;
     private final CountryRepository countryRepository;
+    private final CountryPanelRepository countryPanelRepository;
 
     public ElectricityMetricsServiceImpl(CarbonIntensityRepository carbonIntensityRepository,
                                          ElectricityBreakdownRepository electricityBreakdownRepository,
                                          ElectricityMetricsMapper electricityMetricsMapper,
-                                         CountryRepository countryRepository) {
+                                         CountryRepository countryRepository, CountryPanelRepository countryPanelRepository) {
         this.carbonIntensityRepository = carbonIntensityRepository;
         this.electricityBreakdownRepository = electricityBreakdownRepository;
         this.electricityMetricsMapper = electricityMetricsMapper;
         this.countryRepository = countryRepository;
+        this.countryPanelRepository = countryPanelRepository;
     }
 
     @Override
@@ -67,7 +68,6 @@ public class ElectricityMetricsServiceImpl implements ElectricityMetricsService 
 
     @Override
     public CountryDetailDTO getElectricityDataByCountry(String countryCode) {
-        Country country = getCountryOrThrow(countryCode);
 
         ElectricityBreakdown breakdown = electricityBreakdownRepository.findByZone(countryCode)
                 .orElseThrow(() -> new DataNotFoundException("Electricity breakdown not found for: " + countryCode));
@@ -75,35 +75,33 @@ public class ElectricityMetricsServiceImpl implements ElectricityMetricsService 
         CarbonIntensity intensity = carbonIntensityRepository.findByCountryCode(countryCode)
                 .orElseThrow(() -> new DataNotFoundException("Carbon intensity not found for: " + countryCode));
 
+        Country country = countryRepository.findByCode(countryCode)
+                .orElseThrow(() -> new DataNotFoundException("Country not found for code: " + countryCode));
+
+        List<SolarPanelDTO> countryPanels = countryPanelRepository.findByCountryCode(countryCode)
+                .stream().map(this::mapPanels).collect(Collectors.toList());
+
         CountryDetailDTO dto = electricityMetricsMapper.toDTO(breakdown, intensity, CalculationUtils.formatPopulation(country.getPopulation()));
-        dto.setSolarPanels(mapPanels(country));
+        dto.setSolarPanels(countryPanels);
 
         System.out.println("Computed average metrics for country " + countryCode + "carbon intensity records: " + dto);
         return dto;
     }
 
-    private Country getCountryOrThrow(String countryCode) {
-        return countryRepository.findByCode(countryCode)
-                .orElseThrow(() -> new DataNotFoundException("Country not found for code: " + countryCode));
+    private SolarPanelDTO mapPanels(CountryPanel cp) {
+        Panel p = cp.getPanel();
+        Country c = cp.getCountry();
+        return SolarPanelDTO.builder()
+                .id(cp.getId())
+                .countryCode(c.getCode())
+                .panelName(p.getName())
+                .installationCost(p.getInstallationCost())
+                .productionPerPanel(p.getProductionPerPanel())
+                .description(p.getDescription())
+                .efficiency(p.getEfficiency())
+                .lifespan(p.getLifespan())
+                .temperatureTolerance(p.getTemperatureTolerance())
+                .warranty(p.getWarranty())
+                .build();
     }
-
-    private List<SolarPanelDTO> mapPanels(Country country) {
-        return country.getCountryPanels().stream()
-                .map(cp -> {
-                    Panel p = cp.getPanel();
-                    return SolarPanelDTO.builder()
-                            .id(p.getId())
-                            .panelName(p.getName())
-                            .installationCost(p.getInstallationCost())
-                            .productionPerPanel(p.getProductionPerPanel())
-                            .description(p.getDescription())
-                            .efficiency(p.getEfficiency())
-                            .lifespan(p.getLifespan())
-                            .temperatureTolerance(p.getTemperatureTolerance())
-                            .warranty(p.getWarranty())
-                            .build();
-                })
-                .collect(Collectors.toList());
-    }
-
 }
