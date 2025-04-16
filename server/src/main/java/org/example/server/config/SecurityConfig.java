052d05c181b58f2462f83b54b6aa63feaf6c4605
Beomsu Koh <bkoh3@sheffield.ac.uk>
2025-04-16 14:37:37 +0100

    fix : cors error which comes from preflight

--- server/src/main/java/org/example/server/config/SecurityConfig.java
+++ server/src/main/java/org/example/server/config/SecurityConfig.java
@@ -17,6 +17,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 // Comment this part.
 //                .authorizeHttpRequests(auth -> auth
