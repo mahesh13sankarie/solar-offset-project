b10a6186f3d78070dfdb2859fa764539aa426bd7
Beomsu Koh <bkoh3@sheffield.ac.uk>
2025-04-23 09:48:48 +0100

    fix : stripe typo (should be handled in backend side)

--- client/src/components/LandingPage/Payment.jsx
+++ client/src/components/LandingPage/Payment.jsx
@@ -106,7 +106,7 @@ const Payment = () => {
                 countryPanelId: selectedPanel.id, // Assuming the panel has an id field
                 amount: calculateTotal(),
                 paymentType: "STRIPE",
-                paymentMethodId: `pm_card_${cardNumber.slice(-4)}`, // Just for demonstration
+                paymentMethodId: `pm_card_visa`, // Just for demonstration
             };
             console.log(paymentData);
             // Send payment request to API using axios
