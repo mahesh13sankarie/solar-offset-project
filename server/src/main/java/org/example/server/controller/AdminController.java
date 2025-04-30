82a73b37e2bba9237d49b07ca5a5cc4d44517132
Asti Dhiya Anzaria <adanzaria1@sheffield.ac.uk>
2025-04-30 11:26:29 +0100

    feat: attempt to add restricted api for admin only

--- server/src/main/java/org/example/server/controller/AdminController.java
+++ server/src/main/java/org/example/server/controller/AdminController.java
@@ -1,13 +1,14 @@
 package org.example.server.controller;
 
 import org.example.server.dto.UserRequest;
+import org.example.server.entity.User;
 import org.example.server.mapper.AuthResponseMapper;
 import org.example.server.service.auth.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @author: astidhiyaa
@@ -26,16 +27,12 @@ public class AdminController {
     //Fetch all user data except admin
     @GetMapping("/users")
     public ResponseEntity<?> fetchUser() {
-
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-//
-        return ResponseEntity.ok().body(auth.getPrincipal());
-//        List<User> users = authService.getUsers();
-//        if (users.isEmpty()) {
-//            return ResponseEntity.ok().body(responseMapper.buildErrorMessage("Empty user", 400));
-//        } else {
-//            return ResponseEntity.ok().body(responseMapper.buildCustomResponse(users));
-//        }
+        List<User> users = authService.getUsers();
+        if (users.isEmpty()) {
+            return ResponseEntity.ok().body(responseMapper.buildErrorMessage("Empty user", 400));
+        } else {
+            return ResponseEntity.ok().body(responseMapper.buildCustomResponse(users));
+        }
     }
 
     @PutMapping("/update-role")
