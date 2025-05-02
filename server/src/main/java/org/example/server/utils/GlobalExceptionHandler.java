0f8c3c20dcc8c1058d3985dbee5a176709dafc25
Beomsu Koh <bkoh3@sheffield.ac.uk>
2025-05-02 13:44:12 +0100

    feat : controller test

--- server/src/main/java/org/example/server/utils/GlobalExceptionHandler.java
+++ server/src/main/java/org/example/server/utils/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package org.example.server.utils;
 
+import org.example.server.exception.DataNotFoundException;
 import org.example.server.exception.PaymentException;
 import org.example.server.exception.PanelTransactionException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -24,5 +26,14 @@ public class GlobalExceptionHandler {
 				ex.getCode(),
 				ex.getMessage(),
 				ex.getStatus());
+	}
+
+	// Handler for data not found exceptions
+	@ExceptionHandler(DataNotFoundException.class)
+	public ApiResponse<ApiResponse.CustomBody<Object>> handleDataNotFoundException(DataNotFoundException ex) {
+		return ApiResponseGenerator.fail(
+				"DATA-001", // Using a consistent code pattern
+				ex.getMessage(),
+				HttpStatus.NOT_FOUND);
 	}
 }
